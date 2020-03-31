a68ea98d24dbdf5598c01278385328ecec5210cb
sgneha <sgneha05@gmail.com>
2020-03-31 20:20:07 +0100

    test for displaying multiple notes list view

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,18 +1,37 @@
 
 (function (exports) {
 
-    var noteList = new NoteList()
-    noteList.addNote('testing note')
+
+
     function TestNotelistView() {
+        var noteList = new NoteList()
+        noteList.addNote('testing note')
         var noteListView = new NoteListView(noteList)
         if (noteListView.displayNotes() === ('<ul><li><div>testing note</div></li></ul>')) {
-            console.log('Test for displaying notes passed')
+            console.log('Test for displaying single note passed')
+
         }
         else {
-            console.log('Test for displaying notes failed')
+            console.log('Test for displaying single note failed')
+        }
+
+    }
+    function TestNotelistViewforMultiple() {
+        var noteList = new NoteList()
+        noteList.addNote('testing note 1')
+        noteList.addNote('testing note 2')
+        var noteListView = new NoteListView(noteList)
+
+        if (noteListView.displayNotes() === ('<ul><li><div>testing note 1</div></li><li><div>testing note 2</div></li></ul>')) {
+            console.log('Test for displaying  multiple notes passed')
+
+        }
+        else {
+            console.log('Test for displaying  multiple notes failed')
         }
 
     }
     TestNotelistView()
+    TestNotelistViewforMultiple()
 
 })(this)
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -4,9 +4,11 @@
 
     }
     NoteListView.prototype.displayNotes = function () {
+        var result = []
         for (let i = 0; i < this.noteList.viewAllNotes().length; i++) {
-            return ('<ul>' + `<li><div>${this.noteList.viewAllNotes()[i].text}</div></li>` + '</ul>')
+            result.push(`<li><div>${this.noteList.viewAllNotes()[i].text}</div></li>`)
         }
+        return `<ul>${result.join('')}</ul>`
 
     }
     exports.NoteListView = NoteListView
