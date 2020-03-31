65880de51acc61c10de80ff14342987031ddf16f
sgneha <sgneha05@gmail.com>
2020-03-31 10:48:20 +0100

    added method view note with test

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,16 +1,28 @@
 (function (exports) {
+    var text = "My favourite language is JavaScript"
+    var note = new note(text);
     function testFortext() {
-        var text = "My favourite language is JavaScript"
-        var note = new note(text);
+
         if (note.text === text) {
             console.log("Test for text passed")
         }
         else {
             throw new Error("Test for text failed")
         }
+        function testForViewNote() {
+            if (note.viewNote() === text) {
+                console.log("Test for viewNote passed")
+            }
+            else {
+                throw new Error("Test for viewNote failed")
+            }
+
+        }
 
     }
     testFortext();
+    testForViewNote();
+
 
 })(this)
 
--- src/note-model.js
+++ src/note-model.js
@@ -3,6 +3,9 @@
     function Note(text) {
         this.text = text
     }
+    Note.prototype.viewNote = function () {
+        return this.text
+    }
     exports.Note = Note
 
 })(this)
